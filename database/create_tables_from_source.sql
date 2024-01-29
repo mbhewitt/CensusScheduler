93744735779bbf1bc269a98c7a072d3f40dfbcce
mbhewitt <matthew.hewitt@spin.pm>
2024-01-29 10:39:51 -0800

    create = make something new delete = destroy something completely add = assign something that’s already created remove = unassign something and do not delete following the semantics above, i suggest the following in op_shifts, rename add_shift field to create_shift in op_volunteer_roles, rename delete_role field to remove_role in op_roles, rename add_role field to create_role in op_volunteer_shifts, rename delete_shift field to remove_shift

--- database/create_tables_from_source.sql
+++ database/create_tables_from_source.sql
@@ -5,14 +5,16 @@ create table op_shifts (
         s.subject position, sum(s.qty) as total_slots, sum(case when s.shiftboard_id=0 then s.qty else 0 end) free_slots,
         max(s.role) role,c.category,c.core,c.lead,c.critical,c.RoleCategory position_category,c.SubCategoryPreReq prerequisite,
         c.offPlaya off_playa,c.SubCategory shift_category, concat(s.date,s.event_code) shift_id,concat(s.date,s.event_code,s.subject) shift_position_id,
-        max(details) details, c.WAPpoints wap_points,concat(s.date,' ',left(s.shift,5)) start_time_lt,concat(s.date,' ',
-        right(s.shift,5)) end_time_lt, concat((case when d.datename is null then 'OffPlaya' else d.datename end)," ",c.shortname," ",s.shift) as shiftname,
-        c.shortname,false add_shift,false delete_shift, false update_shift 
+        max(details) details, c.WAPpoints wap_points,
+        concat(s.date,' ',left(s.shift,case when s.shift like '%M%' then 7 else 5 end)) start_time_lt,
+        concat(s.date,' ',right(s.shift,case when s.shift like '%M%' then 7 else 5 end)) end_time_lt, 
+        concat((case when d.datename is null then 'OffPlaya' else d.datename end)," ",c.shortname," ",s.shift) as shiftname,
+        c.shortname
      from shiftboard2 s left join bm_dates d using (date) left join subject_category c using (subject) 
      where left(s.date,4)=left(date_sub(now(),interval 5 month),4)  
      group by 1,2,3,4,5,9,10,11,12,13,14,15,16,17,18,20,21,22,23,24
   );
-alter table op_shifts add primary key(shift_position_id), add key(shift_id), add key (year,off_playa), add notes longtext;
+alter table op_shifts add create_shift boolean default false, add delete_shift boolean default false, add update_shift boolean default false, add primary key(shift_position_id), add key(shift_id), add key (year,off_playa), add notes longtext;
 
 update bm_volunteers v inner join shiftboard_pinfo p using (shiftboard_id) inner join shiftboard_rinfo2 r using (shiftboard_id) 
        set v.playaname=p.playaname,v.first=p.first,v.last=p.last,v.account=r.account;
@@ -28,7 +30,7 @@ create table op_volunteers (
    select v.shiftboard_id,v.newplaya playa_name,concat(v.first,' ',v.last) world_name,p.email,GREATEST(p.mobilephone,p.homephone,p.workphone) phone,
          right(r.view,4) passcode, mid(r.view,locate('account=',r.view)+8,10) account_id, 
          case when r.roles like '%Core Crew%' or r.roles like '%Lead%' then true else false end core_crew, 
-         false new_account,false needs_update, 0 new_shiftboard_id,n.score, z.rs_shifts,z.total_shifts 
+         0 new_shiftboard_id,n.score, z.rs_shifts,z.total_shifts 
       from bm_volunteers v join shiftboard_pinfo p using (shiftboard_id) 
          left join shiftboard_rinfo2 r using (shiftboard_id) left join (select shiftboard_id,avg(score) score from review_notes 
             where date> date_sub(now(),interval 2 year) group by 1) n using (shiftboard_id) 
@@ -37,14 +39,17 @@ create table op_volunteers (
             else (case when subject like '%trainer%' then 1 else 0 end ) end) total_shifts 
             from shiftboard2 where noshow='' and date>date_sub(now(), interval 5 year) and date < now() group by 1) z using (shiftboard_id) 
       where exists (select * from shiftboard2 s2 where s2.shiftboard_id=v.shiftboard_id and date>date_sub(now(), interval 1 year)));
-alter table op_volunteers add notes longtext, add location longtext, add emergency_contact longtext, add primary key(shiftboard_id), 
+alter table op_volunteers add create_volunteer boolean default false,
+        add update_volunteer boolean default false,
+        add delete_volunteer boolean default false,
+        add notes longtext, add location longtext, add emergency_contact longtext, add primary key(shiftboard_id), 
 	add key (passcode,shiftboard_id), modify world_name text,modify email text, modify phone text;
 
-insert ignore into op_volunteers (shiftboard_id,playa_name,world_name,email,phone,passcode,account_id,core_crew,new_account,needs_update,
+insert ignore into op_volunteers (shiftboard_id,playa_name,world_name,email,phone,passcode,account_id,core_crew,
                                   new_shiftboard_id,score,rs_shifts,total_shifts) (
    select v.shiftboard_id,v.newplaya playa_name,concat(v.first,' ',v.last) world_name,p.email,GREATEST(p.mobilephone,p.homephone,p.workphone)
          phone,right(r.view,4) passcode, mid(r.view,locate('account=',r.view)+8,10) account_id, case when r.roles like '%Core Crew%' then true else false end
-         core_crew, false new_account,false needs_update, 0 new_shiftboard_id,n.score, z.rs_shifts,z.total_shifts 
+         core_crew, 0 new_shiftboard_id,n.score, z.rs_shifts,z.total_shifts 
       from bm_volunteers v join shiftboard_pinfo p using (shiftboard_id) left join shiftboard_rinfo2 r using (shiftboard_id) 
          left join (select shiftboard_id,avg(score) score from review_notes where date> date_sub(now(),interval 2 year) group by 1) n 
          using (shiftboard_id) left join (select shiftboard_id,sum(case when room='RS' then (case when subject like
@@ -62,7 +67,7 @@ select shiftboard_id, SUBSTRING_INDEX(SUBSTRING_INDEX(roles, ',', numbers.n), ',
    from numbers inner join shiftboard_rinfo2 on CHAR_LENGTH(roles) -CHAR_LENGTH(REPLACE(roles, ',', ''))>=numbers.n-1);
 delete from op_volunteer_roles where roles='';
 delete from op_volunteer_roles where roles in (select role from shiftboard_roles where display=0);
-alter table op_volunteer_roles add primary key(shiftboard_id,roles), add key(roles), add add_role boolean default false, add delete_role boolean default false;
+alter table op_volunteer_roles add primary key(shiftboard_id,roles), add key(roles), add add_role boolean default false, add remove_role boolean default false;
 
 -- drop table if exists op_volunteer_roles;
 -- create table op_volunteer_roles (
@@ -80,7 +85,7 @@ insert into op_volunteer_roles (shiftboard_id,roles) (
    select shiftboard_id,'Admin' from shiftboard_rinfo2 r where r.roles like '%Core Crew%' or r.roles like '%Lead%' or r.roles like '%Training%');
 
 drop table if exists op_roles;
-create table op_roles (roles varchar(50) not null, add_role boolean default false, delete_role boolean default false, display boolean default true, primary key(roles));
+create table op_roles (roles varchar(50) not null, create_role boolean default false, delete_role boolean default false, display boolean default true, primary key(roles));
 
 insert into op_roles (roles,display) select role,display from shiftboard_roles;
 insert ignore into op_roles (roles) select distinct roles from op_volunteer_roles;
@@ -88,11 +93,14 @@ insert ignore into op_roles (roles) select distinct roles from op_volunteer_role
 drop table if exists op_volunteer_shifts;
 create table op_volunteer_shifts (
    select distinct concat(date,event_code,subject) shift_position_id, shiftboard_id,id shiftboard_shift_id, 
-         case when cast(concat(date,' ',right(shift,5)) as datetime)>=now() and noshow='' then 'X' else noshow end noshow, false add_shift,
-         false delete_shift, false update_shift from shiftboard2 where left(date,4)=left(date_sub(now(),interval 5 month),4)
+         case when noshow='' and date>=now() then 'X' else noshow end noshow
+         from shiftboard2 where left(date,4)=left(date_sub(now(),interval 5 month),4)
    );
 alter table op_volunteer_shifts 
    modify shiftboard_shift_id bigint default 0, 
+   add add_shift boolean default false,
+   add remove_shift boolean default false,
+   add update_shift boolean default false,
    add primary key (shiftboard_id,shift_position_id,shiftboard_shift_id), 
    add key (shift_position_id);
 
