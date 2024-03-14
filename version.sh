52ff65eccb44cfcdb376fa37db3a0a0ef176ad1a
mbhewitt <dircery@gmail.com>
2024-03-13 23:50:18 -0700

    version by week/2+41->ASCII

--- version.sh
+++ version.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
-major_version='E'
+if [ "$(uname)" == "Darwin" ]; then
+    WOY=`date -v "+120d" "+%U"`
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    WOY=`date "+%U" -d "+ 120 days"`
+fi
+
+major_version=`echo $((($WOY/2)+41))|xxd -r -p`
+
 username=`git config user.name`
 pullnumber=`git ls-remote origin 'pull/*/head'|awk -F/ '{print $3}'|sort -n|tail -n1|awk '{printf("%05d\n", $1+1)}'`
 if [ "$(uname)" == "Darwin" ]; then
