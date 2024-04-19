ef32fbba8c0e172d0e98e6b0e16d8be200f4c920
Randy Reyes <rqreyes@gmail.com>
2024-04-18 23:46:16 -0700

    feat: add role check for create shift page

--- app/pages/create-shift/index.tsx
+++ app/pages/create-shift/index.tsx
@@ -1,8 +1,24 @@
 import Head from "next/head";
+import { useContext } from "react";
 
 import { CreateShift } from "src/components/shifts/CreateShift";
+import { SignIn } from "src/components/sign-in";
+import { SessionContext } from "src/state/session/context";
+import { checkIsSuperAdmin } from "src/utils/checkIsRoleExist";
 
 const CreateShiftPage = () => {
+  // context
+  // --------------------
+  const {
+    sessionState: {
+      user: { roleList },
+    },
+  } = useContext(SessionContext);
+
+  // logic
+  // --------------------
+  const isSuperAdmin = checkIsSuperAdmin(roleList);
+
   // display
   // --------------------
   return (
@@ -12,7 +28,7 @@ const CreateShiftPage = () => {
         <meta name="description" content="" />
         <link rel="icon" href="/general/favicon.ico" />
       </Head>
-      <CreateShift />
+      {isSuperAdmin ? <CreateShift /> : <SignIn />}
     </>
   );
 };
