f4ea1dc32c6d4453b6af956ca295504e65a93a3d
Randy Reyes <rqreyes@gmail.com>
2024-04-22 12:42:01 -0700

    bump version

--- app/src/components/layout/Footer.tsx
+++ app/src/components/layout/Footer.tsx
@@ -217,7 +217,7 @@ export const Footer = () => {
               color: theme.palette.common.white,
             }}
           >
-            2024.W.00030.Prizmo
+            2024.W.00031.Prizmo
           </Typography>
         </Stack>
       </Container>
