e24c5bf7afdeeed2f8cec46ea77193b92b11a77a
Prizmo <rqreyes@gmail.com>
2024-05-06 21:23:13 -0700

    refactor: move behavioral standards into roles

--- app/src/components/layout/Footer.tsx
+++ app/src/components/layout/Footer.tsx
@@ -217,7 +217,7 @@ export const Footer = () => {
               color: theme.palette.common.white,
             }}
           >
-            2024.X.00039.Prizmo
+            2024.X.00040.Prizmo
           </Typography>
         </Stack>
       </Container>
