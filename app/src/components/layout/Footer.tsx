35c74b873cd68a7879969029e5d3e20f0197baae
Prizmo <rqreyes@gmail.com>
2024-05-08 14:36:32 -0700

    bump version

--- app/src/components/layout/Footer.tsx
+++ app/src/components/layout/Footer.tsx
@@ -211,7 +211,7 @@ export const Footer = () => {
               color: theme.palette.common.white,
             }}
           >
-            2024.X.00040.Prizmo
+            2024.X.00043.Prizmo
           </Typography>
         </Stack>
       </Container>
