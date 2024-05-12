d1cc71c1fd29c827cee78be066898c8c65959435
Prizmo <rreyes@magicleap.com>
2024-05-11 17:50:15 -0700

    bump version

--- app/src/components/layout/Footer.tsx
+++ app/src/components/layout/Footer.tsx
@@ -221,7 +221,7 @@ export const Footer = () => {
               color: theme.palette.common.white,
             }}
           >
-            2024.S.00047.Prizmo
+            2024.S.00048.Prizmo
           </Typography>
         </Stack>
       </Container>
