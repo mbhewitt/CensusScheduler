a8ac93fc3376a62c9771c06665d14f8975a4e877
Prizmo <rqreyes@gmail.com>
2024-06-17 22:45:27 -0700

    feat: disable super admin role removal

--- app/src/components/layout/Footer.tsx
+++ app/src/components/layout/Footer.tsx
@@ -221,7 +221,7 @@ export const Footer = () => {
               color: theme.palette.common.white,
             }}
           >
-            2024.U.00058.Prizmo
+            2024.U.00060.Prizmo
           </Typography>
         </Stack>
       </Container>
