70d0b69618a0c2d7f74c713aff000a386544ea68
mbhewitt <dircery@gmail.com>
2025-05-03 11:20:51 -0700

    CronUpdates

--- cron_update.sh
+++ cron_update.sh
@@ -1,6 +1,14 @@
-cd CensusScheduler
-git checkout main
-git pull
+cd ~/CensusScheduler
+V1_TAG =$(<new_current_version.txt)
+#git checkout main
+#git pull
+V2_TAG =$(<new_current_version.txt)
+DC_found=`docker images|grep $V2_TAG|wc|awk '{print $1}'`
+echo "tags $V1_TAG -> $V2_TAG DC_Tag Found $DC_found"
+
 sudo docker system prune -fa
-sudo docker-compose --file docker-compose-playa.yaml build --no-cache
-sudo docker-compose --file docker-compose-playa.yaml up -d
+sudo docker compose --file docker-compose-playa.yaml build &&
+sudo docker compose --file docker-compose-playa.yaml up -d &&
+sudo docker tag burning-man/census-app:latest burning-man/census-app:$V2_TAG &&
+echo "$V2_TAG update successful"
+sudo docker compose --file docker-compose-playa.yaml up -d
